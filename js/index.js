964f8edc7b6b80822eb256de8f9d69fd986448f5
Catherine Newman <catherine.r.newman@gmail.com>
2015-12-15 21:26:15 +0900

    update javascript

--- js/index.js
+++ js/index.js
@@ -3,64 +3,122 @@ $(document).ready(function() {
     minFont: 12,
     maxFont: 20
   });
-  var testNumLength = function(number) {
-    if (number.length > 13) {
-      totaldiv.text(number.substr(number.length - 13, 13));
-      if (number.length > 15) {
-        number = "";
-        totaldiv.text("Err");
-      }
-    }
-  };
   var number = "";
   var newnumber = [];
   var operator = [];
-  var screendiv = $("#screen");
+  var screendiv = $("#screen-number");
+  var clear = false;
+  var operatorClicked = true;
   screendiv.text("0");
-  $(".number").on("click", (function() {
-    number += $(this).text();
-    screendiv.text(number);
-    testNumLength(number);
-  }))
+  $(".number").on("click", function() {
+    if (number.length < 10) {
+      if (clear === true) {
+        clear = false;
+      }
+      operatorClicked = false;
+      number += $(this).text();
+      screendiv.fadeOut(10, function() {
+        $(this).text(number)
+      }).fadeIn(200);
+    }
+  });
+  $(".decimal").on("click", function() {
+    if (number.length < 9) {
+      if (number.indexOf(".") === -1) {
+        if (clear === true) {
+          clear = false;
+        }
+        operatorClicked = false;
+        number += $(this).text();
+        screendiv.fadeOut(10, function() {
+          $(this).text(number)
+        }).fadeIn(200);
+      }
+    }
+  });
   $(".operator").on("click", function() {
-    if ($(this).hasClass("divide")) {
-      operator.push("/");
+    if (operatorClicked !== true) {
+      if (clear === true) {
+        var clearOperator = operator;
+        operator = clearOperator.slice(0, -1);
+        clear = false;
+      }
+      if ($(this).hasClass("divide")) {
+        operator.push("/");
+        operatorClicked = true;
+      }
+      if ($(this).hasClass("multiply")) {
+        operator.push("*");
+        operatorClicked = true;
+      }
+      if ($(this).hasClass("minus")) {
+        operator.push("-");
+        operatorClicked = true;
+      }
+      if ($(this).hasClass("plus")) {
+        operator.push("+");
+        operatorClicked = true;
+      }
+      if (number !== "") {
+        newnumber.push(number);
+      }
+      number = "";
     }
-    if ($(this).hasClass("multiply")) {
-      operator.push("*");
-    }
-    if ($(this).hasClass("minus")) {
-      operator.push("-");
-    }
-    if ($(this).hasClass("plus")) {
-      operator.push("+");
-    }
-    newnumber.push(number);
-    number = "";
   });
   $(".clear,.clearall").on("click", function() {
     number = "";
     screendiv.text("0");
+    operatorClicked = true;
+    if ($(this).attr("class") === "clear") {
+      clear = true;
+    }
     if ($(this).attr("class") === "clearall") {
       newnumber = [];
+      operator = [];
     }
   });
   $(".equals").on("click", function() {
-    var answer = 0;
-    for (var o = 0; o < operator.length; o++) {
-      if (operator[o] === "+") {
-        number = (parseInt(number, 10) + parseInt(newnumber, 10)).toString(10);
-      } else if (operator[o] === "-") {
-        number = (parseInt(newnumber, 10) - parseInt(number, 10)).toString(10);
-      } else if (operator[o] === "/") {
-        number = (parseInt(newnumber, 10) / parseInt(number, 10)).toString(10);
-      } else if (operator[o] === "*") {
-        number = (parseInt(newnumber, 10) * parseInt(number, 10)).toString(10);
-      }
+    if (operator.length === newnumber.length + number.length) {
+      var clearOperator = operator;
+      operator = clearOperator.slice(0, -1);
     }
-    screendiv.text(number);
-    testNumLength(number);
-    number = "";
-    newnumber = [];
+    if (operator.length > 0) {
+      var answer = "";
+      while (operator.length !== 0) {
+        var tempOperator = operator.shift();
+        if (newnumber.length === 1) {
+          if (tempOperator === "+") {
+            answer = (parseFloat(number, 10) + parseFloat(newnumber[0], 10)).toString(10);
+          } else if (tempOperator === "-") {
+            answer = (parseFloat(newnumber[0], 10) - parseFloat(number, 10)).toString(10);
+          } else if (tempOperator === "/") {
+            answer = (parseFloat(newnumber[0], 10) / parseFloat(number, 10)).toString(10);
+          } else if (tempOperator === "*") {
+            answer = (parseFloat(newnumber[0], 10) * parseFloat(number, 10)).toString(10);
+          }
+        } else {
+          if (tempOperator === "+") {
+            answer = (parseFloat(newnumber.shift(), 10) + parseFloat(newnumber.shift(), 10)).toString(10);
+          } else if (tempOperator === "-") {
+            answer = (parseFloat(newnumber.shift(), 10) - parseFloat(newnumber.shift(), 10)).toString(10);
+          } else if (tempOperator === "/") {
+            answer = (parseFloat(newnumber.shift(), 10) / parseFloat(newnumber.shift(), 10)).toString(10);
+          } else if (tempOperator === "*") {
+            answer = (parseFloat(newnumber.shift(), 10) * parseFloat(newnumber.shift(), 10)).toString(10);
+          }
+          newnumber.unshift(answer);
+        }
+      }
+
+      screendiv.fadeOut(10, function() {
+        if (answer.length > 10) {
+          $(this).text("..." + answer.slice(answer.length - 10, -1));
+        } else {
+          $(this).text(answer);
+        }
+      }).fadeIn(200);
+      number = "";
+      newnumber = [];
+    }
   });
 })
